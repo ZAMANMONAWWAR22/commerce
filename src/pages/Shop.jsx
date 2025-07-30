f8d8f6535914245ae0496f167fd995580c7ee07b
ZAMANMONAWWAR22 <monawwarzaman40@gmail.com>
2025-07-30 08:57:42 +0530

    select option

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Dropdown from '../component/Dropdown';
 
 const Shop = () => {
   const [data, setData] = useState([]);
@@ -14,9 +15,12 @@ const Shop = () => {
 
   return (
     <>
-      <div className='text-2xl font-bold'>Products For You</div>
-      <div className='flex flex-row items-center justify-center bg-blue-300 h-full w-[95%] m-auto'>
-        <div className='flex-1 h-full bg-amber-600 flex items-start'>left area</div>
+      <div className='text-2xl font-bold m-5 ml-10'>Products For You</div>
+      <div className='flex flex-row items-start justify-center bg-blue-600 h-full w-[95%] m-auto'>
+               <div className='flex-1 h-full bg-white flex flex-col items-start justify-start'>
+        <Dropdown/>
+        
+        </div>
         <div className='flex-3 h-full bg-white justify-center items-center flex flex-wrap gap-4 p-4'>
           {data.map((item) => (
             <a href={`/product/${item.id}`} key={item.id} className="no-underline text-black">
@@ -24,7 +28,7 @@ const Shop = () => {
                 <img src={item.thumbnail} alt={item.title} className="w-full h-64 object-cover rounded-t-lg  bg-blue-200" />
                 <div className=" w-full bg-gradient-to-t from-white/90 via-white/70 to-transparent p-4 flex flex-col">
                   <h2 className="text-[18px] font-normal text-gray-500">{item.title.slice(0,28)} {item.title.length>28?'...':''} </h2>
-
+<div>{item.category}</div>
                   <div className="flex items-center justify-between mt-2">
                     <span className="text-lg font-bold text-blue-600"> ₹{Math.round(item.price*84)}</span>
                     <span className="text-sm text-red-500">-{item.discountPercentage}%</span>
